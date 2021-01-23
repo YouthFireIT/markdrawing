e97d218b7d6ee1640e3ed2cea7094e32d9cfad89
yeapes <yeapesh2831@gmail.com>
2021-01-23 16:51:30 +0600

    My account Added

--- routes/web.php
+++ routes/web.php
@@ -498,6 +498,8 @@ Route::post('/web/decrease', 'Frontend\CartsController@decrease_quantity');
 
 
 
+//My Account ==== User Account
+Route::get('user/my-account','Frontend\UserprofileController@index')->name('my-account');
 
 
 //Checkout
