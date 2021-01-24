5b1a2ec19e26a49f84e74ed15fa17de55eee32a8
yeapes <yeapesh2831@gmail.com>
2021-01-24 12:11:17 +0600

    Contact Newsletter, all pages done

---
+++ resources/views/frontend/markdrawing/faq.blade.php
@@ -0,0 +1,76 @@
+@extends('frontend.layouts.master.app')
+ 
+@section('content')
+   <!-- page name banner -->
+	<section id="page_banner_portrait">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="content_wrap_cc">
+					<p class="pt_heading c_center">
+						Markdrawing
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="content_wrap_cc">
+					<p class="pt_heading_t c_center">
+						FAQ
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+    <!-- FAQ -->
+    <section id="faq_part">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="faq_wrap">
+                        <h2>Frequently Asked Questions</h2>
+                        <div class="faq_set">
+                            <h5>Q. How long does your artwork take to complete</h5>
+                            <p>A. The Artwork itself takes approximately 10 to 12 days. Then if you choose to get it printed or on canvas, a further 3 to 5 business days to arrive. </p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. Can you draw from separate pictures?</h5>
+                            <p>A. Yes I can draw from separate pictures, in fact if you have two clear pictures it's even better for me as I can see the details in the face. </p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. Do you have a maximum number of people that you can draw in an Artwork?</h5>
+                            <p>A. The more the merrier! There's no limit to the number of people in a single artwork. You can have it done with as much people as you want.</p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. Can you draw pets?</h5>
+                            <p>A. I love to draw pets, they are priced the same as humans.</p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. Do you do bigger sizes?</h5>
+                            <p>A. No. But if you would like it bigger, then it is best to have it delivered through email and then you can print out or on a canvas any size you want. All drawings are done in high resolution, so you won't have any problems with the quality. </p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. What types of payment do you accept?</h5>
+                            <p>A. We accept payments through card.</p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. Can I pay cash when my drawing is delivered?</h5>
+                            <p>A. At the moment we do not offer Cash payment upon delivery of your drawing but you can chose from any of our electronic payment options. Payment is required before we commence with the drawing. </p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. How are these drawn?</h5>
+                            <p>A. These are all hand drawn on paper and then scanned and printed either on photo grade paper or on canvas, to any size you want depending on your preference. The Digital styles of drawings that we offer are hand drawn on a tablet connected to a computer.</p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. Can you edit the picture if I dont like it?</h5>
+                            <p>A. As these take a long time to draw from scratch we do not edit the pictures as a rule. However if there is something majorly wrong with the picture of course it will be adjusted. However the final decision will come down to Charlie.</p>
+                        </div>
+                        <div class="faq_set">
+                            <h5>Q. How are they sent?</h5>
+                            <p>A. Canvases and Prints are sent using various services including Myhermes, Royal Mail & APC. This depends on your location.</p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
